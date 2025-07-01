f485bfc0fed3a19b6c7f5ca3004d6d3cbd18704e
Milk0621 <wltjs5360@gmail.com>
2025-07-01 20:34:44 +0900

    feat: 부트스트랩 적용 및 메인 페이지 레이아웃 구성
    - react-bootstrap 설치 및 기본 레이아웃 적용
    - Navbar, Container, Row, Col 컴포넌트를 사용해 메인 상품 리스트 UI 구성
    - 임시 이미지 및 상품 정보 표시

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import {Button, Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+
+      <Container fluid>
+        <Row>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
