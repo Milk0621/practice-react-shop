e16622161788ef32620b9a1980328d79ec69bd42
Milk0621 <wltjs5360@gmail.com>
2025-07-02 17:56:31 +0900

    feat: Detail 페이지에 useEffect 활용 기능 추가
    - 페이지 마운트 시 2초 후 할인 알림창 자동 숨김 처리 및 clearTimeout으로 메모리 누수 방지
    - input 값 변경 감지하여 숫자가 아닐 경우 경고창 출력
    - 기존 alert 상태 변수명을 showAlert로 변경하여 window.alert와 충돌 방지

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,21 +1,59 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+
 
 function Detail( props ) {
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setShowAlert(false); }, 2000);
+        return ()=>{
+            clearTimeout(a);            
+        }
+    }, [])
+    
+    let [text, setText] = useState('');
+    useEffect(()=>{
+        if(isNaN(text)){
+            alert("경고! 숫자만 입력하세요.");
+        }
+    }, [text])
+
+    // useEffect(()=>{  }) 1. 재렌더링마다 코드 실행 
+    // useEffect(()=>{  }, []) 2. mount시 1회 코드 실행 / 5. 특정 state 변경시에만 실행하려면 [state명]
+    // useEffect(()=>{  
+    //  return ()=>{
+    //       3. unmount시 1회 코드 실행 
+    //       4. useEffect 실행 전에 뭔가 실행하려면 언제나 return ()=>{}
+    //  }
+    // }, [])
+    
+    let [count, setCount] = useState(0);
+    let [showAlert, setShowAlert] = useState(true);
 
     let {id} = useParams();
     let shoe = props.shoes.find((x)=> x.id == id)
 
     return(
         <div className="container">
+            {
+                showAlert == true
+                ?   <div className="alert alert-warning">
+                        2초이내 구매시 할인
+                    </div>
+                : null
+            }
             <div className="row">
                 <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
-                <h4 className="pt-5">{shoe.title}</h4>
-                <p>{shoe.content}</p>
-                <p>{shoe.price}</p>
-                <button className="btn btn-danger">주문하기</button> 
+                    <input type="text" onChange={(e)=>{setText(e.target.value)}}/>
+                    <h4 className="pt-5">{shoe.title}</h4>
+                    <p>{shoe.content}</p>
+                    <p>{shoe.price}</p>
+                    <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
         </div> 
