c75eaa20f283b9eecf52952ad77531acccc0f928
Milk0621 <wltjs5360@gmail.com>
2025-07-03 13:32:09 +0900

    feat: Detail 페이지 탭 기능 및 마운트/탭 전환 애니메이션 구현
    - react-bootstrap Nav를 활용한 탭 UI 구현
    - 탭 전환 시 useEffect와 setTimeout을 사용한 페이드 애니메이션 적용
    - 페이지 마운트 시 애니메이션 클래스 적용 및 클린업 처리 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
-
+import { Nav } from 'react-bootstrap';
 
 
 function Detail( props ) {
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setFade('end') }, 100);
+        return ()=>{
+            clearTimeout(a);
+            setFade('');
+        }
+    }, [])
 
     useEffect(()=>{
         let a = setTimeout(()=>{ setShowAlert(false); }, 2000);
@@ -28,14 +35,17 @@ function Detail( props ) {
     //       4. useEffect 실행 전에 뭔가 실행하려면 언제나 return ()=>{}
     //  }
     // }, [])
-    
+    let [fade, setFade] = useState('');
+
     let [showAlert, setShowAlert] = useState(true);
 
     let {id} = useParams();
     let shoe = props.shoes.find((x)=> x.id == id)
 
+    let [tab, setTab] = useState(0);
+
     return(
-        <div className="container">
+        <div className={"container start " + fade}>
             {
                 showAlert == true
                 ?   <div className="alert alert-warning">
@@ -48,15 +58,53 @@ function Detail( props ) {
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <input type="text" onChange={(e)=>{setText(e.target.value)}}/>
+                    <input type="text" placeholder="수량" onChange={(e)=>{setText(e.target.value)}}/>
                     <h4 className="pt-5">{shoe.title}</h4>
                     <p>{shoe.content}</p>
                     <p>{shoe.price}</p>
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
+
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(0) }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(1) }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(2) }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab}/>
         </div> 
+
+        
     )
+}
+function TabContent({tab}){
+
+    let [fade, setFade] = useState('');
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setFade('end') }, 100)
+        return ()=>{
+            clearTimeout(a)
+            setFade('')
+        }
+    }, [tab])
+    
+    return (
+        <div className={"start " + fade}>
+            {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}
+        </div>
+    )
+        
+    // if(tab == 0) return <div>내용0</div>
+    // else if(tab == 1) return <div>내용1</div>
+    // else if(tab == 2) return <div>내용2</div>
+
 }
 
 export default Detail;
